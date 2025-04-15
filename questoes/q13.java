1bc0d7b318cf243133b943bbd4b8e003f78b216a
Osotog <octaviofernandes.tdai.of@gmail.com>
2025-04-15 19:56:13 -0300

    15.04.25

---
+++ questoes/q13.java
@@ -0,0 +1,14 @@
+package prova;
+
+import java.util.Scanner;
+
+public class q4 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("digite uma frase");
+        String frase = teclado.nextLine();
+
+    }
+}
